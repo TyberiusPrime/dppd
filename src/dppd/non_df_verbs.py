e2c50efa8097f510e538e173253a13bd23527347
Unknown <finkernagel@imt.uni-marburg.de>
2021-07-16 13:06:53 +0200

    added collections.Counter.to_frame()

---
+++ src/dppd/non_df_verbs.py
@@ -0,0 +1,11 @@
+from .base import register_verb, register_type_methods_as_verbs
+import pandas as pd
+import collections
+
+
+@register_verb(name="to_frame", types=collections.Counter)
+def collection_counter_to_df(counter):
+    """Turn a collections.Counter into a DataFrame
+    with two columns: key & count
+    """
+    return pd.DataFrame({"key": list(counter.keys()), "counts": list(counter.values())})
