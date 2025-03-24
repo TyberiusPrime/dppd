a6e65d8bda8c688e9ce3046d06544672e97dd49b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2025-03-24 14:49:23 +0100

    convert to pyproject.toml, uv2nix

--- flake.nix
+++ flake.nix
@@ -1,85 +1,73 @@
 {
-  description = "Wraps mbf-bam into an mach-nix importable builder";
-
+  description = "dppd development falke";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    uv2nix.url = "github:/adisbladis/uv2nix";
+    uv2nix.inputs.nixpkgs.follows = "nixpkgs";
+    pyproject-nix.url = "github:/nix-community/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+    uv2nix.inputs.pyproject-nix.follows = "pyproject-nix";
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #uv2nix_hammer_overrides.url = "github:TyberiusPrime/uv2nix_hammer_overrides";
   };
-
   outputs = {
-    self,
     nixpkgs,
+    uv2nix,
+    pyproject-build-systems,
+    #uv2nix_hammer_overrides,
+    ...
   }: let
-    system = "x86_64-linux";
-    overlays = [];
-    pkgs = import nixpkgs {inherit system overlays;};
-  in let
-    palettable = let
-      p = pkgs.python39Packages;
-    in
-      p.buildPythonPackage rec {
-        pname = "palettable";
-        version = "3.3.0";
-        # buildInputs = [p.pandas];
-        propagatedBuildInputs = [p.numpy p.setuptools];
-        src = p.fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-cv7Kcc99eYMM1tkYGwLt8ie4Z9UDvslTz5+pG/RIlr0=";
-        };
-      };
+    pyproject-nix = uv2nix.inputs.pyproject-nix;
+    workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
 
-    mizani = let
-      p = pkgs.python39Packages;
-    in
-      p.buildPythonPackage rec {
-        pname = "mizani";
-        version = "0.8.1";
-        # buildInputs = [p.pandas];
-        propagatedBuildInputs = [p.numpy palettable p.pandas p.matplotlib p.scipy];
-        src = p.fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-itCg76UvG830H2dbZKjA980k52PVO6ztZhPyC9btSSg=";
-        };
-        patchPhase = ''
-          sed -i '3 a version=${version}' setup.cfg
-        '';
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
 
-        doCheck = false;
-      };
-    plotnine = let
-      p = pkgs.python39Packages;
-    in
-      p.buildPythonPackage rec {
-        pname = "plotnine";
-        version = "0.10.1";
-        buildInputs = [mizani];
-        propagatedBuildInputs = [p.pandas p.statsmodels mizani];
-        src = p.fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-2RKgS2ONz4IsUaZ4i4VmQjI0jVFfFR2zpkwAAZZvaEE=";
-        };
-        doCheck = false;
-      };
+    # Generate overlay
+    overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "wheel";
+    };
+    pyprojectOverrides = final: prev: {};
+    #pyprojectOverrides = final: prev: {} ;
+    interpreter = pkgs.python312;
+    spec = {
+      dppd = ["dev"];
+    };
 
-    mypython = pkgs.python39.withPackages (p: [
-      #todo: figure out how to derive this from pyproject.toml
-      p.pytest
-      p.pytest-mock
-      p.pytest-cov
-      p.wrapt
-      p.pandas
-      p.numpy
-      p.natsort
-      plotnine
-    ]);
+    # Construct package set
+    pythonSet' =
+      (pkgs.callPackage pyproject-nix.build.packages {
+        python = interpreter;
+      })
+      .overrideScope
+      (
+        pkgs.lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+          pyprojectOverrides
+        ]
+      );
+
+    # Override host packages with build fixups
+    pythonSet = pythonSet'.pythonPkgsHostHost.overrideScope pyprojectOverrides;
+    virtualEnv = pythonSet.mkVirtualEnv "dppd-ven" spec;
   in {
-    # pass in nixpkgs, mach-nix and what you want it to report back as a version
+    #packages.x86_64-linux.default = defaultPackage;
     devShell.x86_64-linux = pkgs.mkShell {
-      # supplx the specific rust version
-      # be sure to set this back in your build scripts,
-      # otherwise pyo3 will get recompiled all the time
-      nativeBuildInputs = [
-        mypython
+      packages = [
+        virtualEnv
       ];
+      shellHook = ''
+        # Undo dependency propagation by nixpkgs.
+        unset PYTHONPATH
+      '';
     };
   };
 }
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from dppd!")
+
+
+if __name__ == "__main__":
+    main()
