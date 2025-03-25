4c60eca1e43d2400390f6f9d74c501e455c4339a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2025-03-25 09:20:24 +0100

    correct license key in pyproject.toml, bump version for that

--- src/dppd/__init__.py
+++ src/dppd/__init__.py
@@ -4,6 +4,6 @@ from .base import dppd, register_verb, register_type_methods_as_verbs
 from . import single_verbs  # noqa:F401
 from . import non_df_verbs  # noqa:F401
 
-__version__ = "0.30"
+__version__ = "0.31"
 
 __all_ = [dppd, register_verb, register_type_methods_as_verbs, __version__]
